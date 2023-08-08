7ea318c8025c46ea7bdd303623c1e099a58b50af
exhibit19 <exhibit4real@gmail.com>
2023-08-08 03:12:07 +0100

    added homeController test class

---
+++ src/test/java/com/bitscoder/springsocial2cloud/HomeControllerTest.java
@@ -0,0 +1,22 @@
+package com.bitscoder.springsocial2cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldDisplayHomepage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
